f2fe5fada00c470fc0b25348724bb7ce4c9e77b7
wakameritahi <nyankoika@gmail.com>
2026-08-06 09:43:49 +0900

    未完成

---
+++ js.js
@@ -0,0 +1,35 @@
+// 選択した画像ファイルのプレビューを生成する関数を定義します。
+        function preview_f(obj) {
+            // 選択した全てのファイルに対してループ処理をします。
+            for (i = 0; i < obj.files.length; i++) {
+                // FileReaderオブジェクトを作成します。これによりブラウザ上でファイルを読み込むことが可能になります。
+                let fileReader = new FileReader();
+                // onloadイベントハンドラを設定します。ファイルが正常に読み込まれたときに呼び出されます。
+                fileReader.onload = ((e)=> {
+                    // 読み込んだ画像ファイルをData URLとしてimg要素に設定します。
+                    // これにより、選択した画像がブラウザ上でプレビュー表示されます。
+                    document.querySelector('#img_f').innerHTML += '<img src="' + e.target.result + '">';
+                });
+                // ファイルをData URLとして読み込みます。
+                fileReader.readAsDataURL(obj.files[i]);
+            }
+        }
+
+        function preview_l(obj) {
+            // 選択した全てのファイルに対してループ処理をします。
+            for (i = 0; i < obj.files.length; i++) {
+                // FileReaderオブジェクトを作成します。これによりブラウザ上でファイルを読み込むことが可能になります。
+                let fileReader = new FileReader();
+                // onloadイベントハンドラを設定します。ファイルが正常に読み込まれたときに呼び出されます。
+                fileReader.onload = ((e)=> {
+                    // 読み込んだ画像ファイルをData URLとしてimg要素に設定します。
+                    // これにより、選択した画像がブラウザ上でプレビュー表示されます。
+                    document.querySelector('#img_l').innerHTML += '<img src="' + e.target.result + '">';
+                });
+                // ファイルをData URLとして読み込みます。
+                fileReader.readAsDataURL(obj.files[i]);
+            }
+        }
+
+
+//resizeTo(800,600);
